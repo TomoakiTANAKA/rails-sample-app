11fffe2cadd447314e431d24ab293e5d1545ca4b
TomoakiTANAKA <tanaka1987@gmail.com>
2018-04-07 20:00:07 +0900

    404ページにいくようにapplicaton_controllerを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
 
   def hello
     text = 'Hello World'
     render plain: text
+  end
+
+  def not_found
+    render 'errors/404', status: 404
   end
 end
